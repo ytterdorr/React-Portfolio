420dec1f02bef5eb68b80ce6e148b67b69d80c4b
Tore Haglund <ytterdorr@gmail.com>
2018-08-31 09:42:48 +0200

    first tabs with react-tabs

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from './components/hello';
+
+
+import 'react-tabs/style/react-tabs.css';
+import Tabs from './components/tabs';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Hello />
+      <Tabs />
       </div>
     );
   }
